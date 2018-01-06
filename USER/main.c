729a89f86a90c300f3c2d8aa006d84006eabe35c
BinAddress <1525997032@qq.com>
2018-01-06 13:58:16 +0800

    BMP180 温度正常，气压正常

--- USER/main.c
+++ USER/main.c
@@ -13,8 +13,8 @@
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	
 	uart1_init(9600);//debug
-	//uart4_init(9600);//SIM
-	uart5_init(9600);//GPS
+	uart4_init(9600);//SIM
+	//uart5_init(9600);//GPS
 	
 	I2C1_Init();
 	
@@ -29,9 +29,9 @@
 
 		//Read_GPS();
 		
-		//BMP180_Read();
+		BMP180_Read();
 		
-		//delay_ms(500);
+		delay_ms(500);
 		
 	}
 	
